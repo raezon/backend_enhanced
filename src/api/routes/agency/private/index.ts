f339fad3b008050b434e468876ad8af503a8c432
WalidAra <arawalid90@gmail.com>
2025-06-08 16:38:06 +0100

    fix: fixed endpoint products

--- src/api/routes/agency/private/index.ts
+++ src/api/routes/agency/private/index.ts
@@ -19,7 +19,7 @@ router.get("/", getAllAgencies);
 router.get("/:id", idValidator("id", "Agency ID"), getAgencyById);
 router.post("/:id/accounting", idValidator("id", "Agency ID"), createNewAccounting);
 router.post("/:id/authoration", idValidator("id", "Agency ID"), createAuthorization);
-router.post("/:id/product", idValidator("id", "Agency ID"), createAgencyProduct);
+router.post("/:id/products", idValidator("id", "Agency ID"), createAgencyProduct);
 router.post("/:id/b2c", idValidator("id", "Agency ID"), createB2b);
 router.post("/:id/logo", idValidator("id", "Agency ID"), upload.single("logo"), addAgencyLogo);
 
