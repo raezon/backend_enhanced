01025c03877d4db347afe19fdc496a23597f65f3
WalidAra <arawalid90@gmail.com>
2025-06-09 10:31:44 +0100

    feat: added country offer query

--- src/api/routes/country/private/index.ts
+++ src/api/routes/country/private/index.ts
@@ -2,8 +2,25 @@ import { prisma } from "@/config";
 import express, { Request, Response } from "express";
 const router = express.Router();
 
-router.get("", async (_req: Request, res: Response) => {
-    const data = await prisma.country.findMany();
+router.get("", async (req: Request, res: Response) => {
+    const offers = req.query.offers === "true";
+
+    let data: any[] = [];
+
+    if (offers) {
+        data = await prisma.country.findMany({
+            where: {
+                visa: {
+                    some: {}, // means at least one visa
+                },
+            },
+            include: {
+                visa: true, // include visa data if needed
+            },
+        });
+    } else {
+        data = await prisma.country.findMany();
+    }
 
     res.status(200).json({
         message: "got all countries",
