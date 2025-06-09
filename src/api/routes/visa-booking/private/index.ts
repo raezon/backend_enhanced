6a5aeb2077793a2959063a356d21e2e1e7556a64
WalidAra <arawalid90@gmail.com>
2025-06-09 11:55:15 +0100

    fix: fixed routing params

--- src/api/routes/visa-booking/private/index.ts
+++ src/api/routes/visa-booking/private/index.ts
@@ -13,8 +13,8 @@ const {
 
 router.post("", fileUploadMiddleware, createVisaBooking);
 router.get("", getAllVisaBookings);
-router.get(":id", idValidator("id", "Visa Request ID"), getVisaBookingById);
-router.delete(":id", idValidator("id", "Visa Request ID"), deleteVisaBooking);
-router.put(":id", fileUploadMiddleware, idValidator("id", "Visa Request ID"), updateVisaBooking);
+router.get("/:id", idValidator("id", "Visa Request ID"), getVisaBookingById);
+router.delete("/:id", idValidator("id", "Visa Request ID"), deleteVisaBooking);
+router.put("/:id", fileUploadMiddleware, idValidator("id", "Visa Request ID"), updateVisaBooking);
 
 export default router;
